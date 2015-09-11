3154e3a35c11a7955cd0ff5f16e753feac207cfc
Sam Martinez <sammartinez19@gmail.com>
2015-09-11 11:15:38 -0700

    have passing tests starting on webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,23 @@
 
 var findReplace = function(str, findWord, replaceWord){
 
+    //The method replace takes a word the user inputs and replaces it with the selected word
     var newString = str.replace(findWord, replaceWord);
+
+    //Returns the new string with the new word inputted into the list.  
     return newString;
-    
-    // //Splits up the string the user inputs
-    // var arraySplit = str.split(" ");
-    //
-    // /*Within the for loop, it'll go through each string split to check and see if it equals the findWord the
-    //  user wants to replace. Then if it does, it will take the replaceWord and replace it*/
-    // for(var i = 0; i < arraySplit.length; i++) {
-    //
-    //     /*In case a user inputs a letter with a capitial letter it
-    //     convert it back to all lowercase in order to have the test run */
-    //     if(arraySplit[i].toLowerCase() === findWord[i].toLowerCase()) {
-    //
-    //         arraySplit[i] = replaceWord;
-    //     }
-    // }
-    // return arraySplit;
-
-
 }
 
-// $(document).ready(function() {
-//     $("form#findReplace").submit(function(event){
-//         var phrase = ($("input#phrase").val());
-//         var result = findReplace(phrase);
-//
-//         $(".answer").text(result);
-//         $("#result").show();
-//         event.preventDefault();
-//     });
-// });
+$(document).ready(function() {
+    $("form#findReplace").submit(function(event){
+        var str = ($("input#str").val());
+        var findWord = ($("input#findWord").val());
+        var replaceWord = ($("input#replaceWord").val());
+
+        var result = findReplace(str, findWord, replaceWord);
+
+        $(".answer").text(result);
+        $("#result").show();
+        event.preventDefault();
+    });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,11 @@
 
 describe('findReplace', function() {
-    // it("will convert a string into a array", function(){
-    //     expect(findReplace('i like apples','apples', 'oranges')).to.be.a('array');
-    // });
-
-    it("will replace a word within a string to a word you input", function(){
+    it("will replace a word within a string to a word user inputs", function(){
         expect(findReplace("Hello world", "world", "universe")).to.equal("Hello universe");
     });
 
+    it("will replace multiple words within the string that matches the words the user wants to change", function() {
+        expect(findReplace("Give me all the tacos", "Give me", "Feed me")).to.equal("Feed me all the tacos");
+    });
+    
 });
