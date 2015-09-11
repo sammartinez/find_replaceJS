0fc2b8ea703edb91bd5718dd8a328be80df75cb1
Sam Martinez <sammartinez19@gmail.com>
2015-09-11 11:40:53 -0700

    verify that all variables are named descriptive

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
-//JavaScript Function used for this Code Review
-var findReplace = function(str, findWord, replaceWord){
+ //JavaScript Function used for this Code Review
+    var findReplace = function(string, findWord, replaceWord){
 
-    //The method replace takes a word the user inputs and replaces it with the selected word
-    var newString = str.replace(findWord, replaceWord);
+        //The method replace takes a word the user inputs and replaces it with the selected word
+        var newString = string.replace(findWord, replaceWord);
 
-    //Returns the new string with the new word inputted into the list.
-    return newString;
-}
+        //Returns the new string with the new word inputted into the list.
+        return newString;
+    }
 
-//JQuery used in order to have the page run
-$(document).ready(function() {
-    $("form#findReplace").submit(function(event){
-        var str = ($("input#str").val());
-        var findWord = ($("input#findWord").val());
-        var replaceWord = ($("input#replaceWord").val());
+    //JQuery used in order to have the page run
+    $(document).ready(function() {
+        $("form#findReplace").submit(function(event){
+            var string = ($("input#str").val());
+            var findWord = ($("input#findWord").val());
+            var replaceWord = ($("input#replaceWord").val());
 
-        var result = findReplace(str, findWord, replaceWord);
+            var result = findReplace(string, findWord, replaceWord);
 
-//Results displayed are the original sentence and the new sentence
-        $(".original").text(str);
-        $(".answer").text(result);
+    //Results displayed are the original sentence and the new sentence
+            $(".original").text(string);
+            $(".answer").text(result);
 
-        $("#result").show();
-        event.preventDefault();
+            $("#result").show();
+            event.preventDefault();
+        });
     });
-});
