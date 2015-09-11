064052e98b0a2d4c9e362be859357a973f669bd2
Sam Martinez <sammartinez19@gmail.com>
2015-09-11 11:29:04 -0700

    add final comments before READ.md file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
-
+//JavaScript Function used for this Code Review
 var findReplace = function(str, findWord, replaceWord){
 
     //The method replace takes a word the user inputs and replaces it with the selected word
     var newString = str.replace(findWord, replaceWord);
 
-    //Returns the new string with the new word inputted into the list.  
+    //Returns the new string with the new word inputted into the list.
     return newString;
 }
 
+//JQuery used in order to have the page run
 $(document).ready(function() {
     $("form#findReplace").submit(function(event){
         var str = ($("input#str").val());
@@ -16,7 +17,10 @@ $(document).ready(function() {
 
         var result = findReplace(str, findWord, replaceWord);
 
+//Results displayed are the original sentence and the new sentence
+        $(".original").text(str);
         $(".answer").text(result);
+
         $("#result").show();
         event.preventDefault();
     });
